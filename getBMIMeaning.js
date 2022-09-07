3714295f886b7ef7a2d7400f8b4ec09e52c73b79
Thanawat Chen <thanawat.th@mail.kmutt.ac.th>
2022-09-07 20:24:19 +0700

    v1

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -1,9 +1,14 @@
 const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
-  //code here
+  return BMI = weight/(height*height)
 }
 function getBMIMeaning(weight, height) {
-  //code here
+  if (BMI > 25)
+        console.log("Overweight")
+    else if(BMI > 18.5)
+        console.log("Normal weight")
+    else
+        console.log("Underweight")
 }
 module.exports = getBMIMeaning
